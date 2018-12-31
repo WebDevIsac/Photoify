b135e0c4955df93d0b7b444bae0b7f331cc67cad
WebDevIsac <isac.code@gmail.com>
2018-12-31 15:51:57 +0100

    Added php functions

--- home.php
+++ home.php
@@ -1,5 +1,8 @@
 <?php require __DIR__.'/views/header.php' ?>
 
-	<a href="app/users/logout.php" class="logout">Logout</a>
+	<div class="content">
+	
+	</div>
 
+		<a href="app/users/logout.php" class="logout">Logout</a>
 <?php require __DIR__.'/views/footer.php' ?>
